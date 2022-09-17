0f41db767817917616437eaeccad26fa9123c7e9
André Sernajoto <andre.sernajoto@gmail.com>
2022-09-17 01:49:42 -0300

    exercise 3 added

---
+++ lista-de-exercicios-1/exerc3.js
@@ -0,0 +1,7 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+const calcularExpoent = function(base, expoente) {
+    return Math.pow(base, expoente)
+}
+
+console.log(calcularExpoent(5, 3))
